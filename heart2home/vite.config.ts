4e98c411cabdb67e9c3e8d2403747d51a3a0ccf5
Metilda Chee <metildachee@gmail.com>
2025-08-17 23:10:54 +0800

    Update vite.config.ts.

--- heart2home/vite.config.ts
+++ heart2home/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/heart2home/', // This is critical for GitHub Pages subdirectory
+  base: '/heart2home/', // Must match your subdirectory
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
