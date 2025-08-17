c7b927afa12ac5fa7b909f7a1ed338d92351f704
Metilda Chee <metildachee@gmail.com>
2025-08-18 00:34:32 +0800

    Update heart2home.

--- heart2home/vite.config.ts
+++ heart2home/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/heart2home/',
+  optimizeDeps: {
+    exclude: ['fsevents'],   // 👈 ignore macOS-only dep
+  },
+  build: {
+    rollupOptions: {
+      external: ['fsevents'], // 👈 don’t try to bundle it
+    },
+  },
 })
