e3ba6783449b6f7af951b1ac69f68ae6f3d875d4
Metilda Chee <metildachee@gmail.com>
2025-08-18 01:17:00 +0800

    Change type.

--- heart2home/vite.config.ts
+++ heart2home/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/heart2home/dist/',
+  base: 'heart2home/',
   optimizeDeps: {
     exclude: ['fsevents'],   // 👈 ignore macOS-only dep
   },
